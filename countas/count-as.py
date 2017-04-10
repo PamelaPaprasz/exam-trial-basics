2d2cee93fe01f10293183b0fea3085965e0a2c62
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-10 10:38:20 +0200

    (feat): count as function returns 0 when there is no valid file but it does not count letter 'a' at all

--- countas/count-as.py
+++ countas/count-as.py
@@ -1,8 +1,22 @@
-def count_as(change_this):
-    pass
-    # Create a function that takes a filename as string parameter,
-    # counts the occurances of the letter "a", and returns it as a number.
-    # If the file does not exist, the function should return 0 and not break.
+
+def count_as(my_file):
+    try:
+        my_file = open(my_file, 'r')
+        data = my_file.readlines()
+        characters = 0
+        
+        for i in data:
+            if i == 'a':
+                characters += 1            
+        return characters
+        
+    except FileNotFoundError:
+        return 0
 
 print(count_as("afile.txt")) # should print 28
 print(count_as("not-a-file")) # should print 0
+
+
+# Create a function that takes a filename as string parameter,
+# counts the occurances of the letter "a", and returns it as a number.
+# If the file does not exist, the function should return 0 and not break.
