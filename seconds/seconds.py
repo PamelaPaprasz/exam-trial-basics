a50850e0c345dccd9cae7a96469510c42f4dbc18
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-10 10:01:19 +0200

    (feat):function named second is ready and it counts the elements from the first index of the list and returns back every second element

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -1,7 +1,12 @@
 def seconds(change_this):
-    pass
+    
+    change_this = change_this[1:]
+    change_this = change_this[::2]
+    return change_this
+    
+print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
+
     # Create a function that takes a list as a parameter,
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
 
-print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
